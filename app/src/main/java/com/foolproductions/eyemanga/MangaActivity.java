0ce8f11cba9bad806ce2e5d4bbf529461fd3ac4c
DESKTOP-K49PGQT\MarcosMourão <marcosarruda.m@gmail.com>
2019-05-02 21:01:32 -0300

    Usando UnescapeHtml para traduzir os textos da API

--- app/src/main/java/com/foolproductions/eyemanga/MangaActivity.java
+++ app/src/main/java/com/foolproductions/eyemanga/MangaActivity.java
@@ -3,10 +3,13 @@ package com.foolproductions.eyemanga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import com.foolproductions.eyemanga.mangaEdenApi.Manga;
 import com.foolproductions.eyemanga.mangaEdenApi.MangaManager;
+
+import org.apache.commons.text.StringEscapeUtils;
 
 public class MangaActivity extends AppCompatActivity {
 
@@ -23,7 +26,7 @@ public class MangaActivity extends AppCompatActivity {
         MangaManager.setFetchMangaListener(new MangaManager.FetchMangaListener() {
             @Override
             public void onSuccess(Manga manga) {
-                tvTitle.setText(manga.getDescription());
+                tvTitle.setText(StringEscapeUtils.unescapeHtml4(manga.getDescription()));
             }
 
             @Override
